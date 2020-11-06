576598ac13f2246ed8bc342c6d58ea568f19e141
Leonardo Santos <leonardosanttos0077@gmail.com>
2020-11-05 23:07:09 -0300

    Criando um component

---
+++ ex20/src/components/example.js
@@ -0,0 +1,7 @@
+export const exemple = () => {
+    const exempleComponent = document.createElement('div');
+    exempleComponent.innerHTML = `
+        <p>Hi, i'm a exemple component :) </p>`
+
+    return exempleComponent
+}
--- ex20/src/index.js
+++ ex20/src/index.js
@@ -1,1 +1,4 @@
-alert('Hello world!')
+import './assets/css/style.css'
+import exemple from './components/example'
+
+document.body.append(exemple())
