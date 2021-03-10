fdb709d44c94bd267b65b162914b89a25e0300ac
unknown <leonardosanttos0077@gmail.com>
2021-03-10 19:56:29 -0300

    using sass to style

--- ex23/webpack.config.js
+++ ex23/webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path')
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports ={
-    mode: 'development',
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+    
+    mode: isDevelopment ? 'development' : 'production',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    devtool: isDevelopment? 'eval-source-map' : 'source-map',
+
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+
     resolve: {
         extensions: ['.jsx', '.js'],
     },
@@ -21,13 +28,19 @@ module.exports ={
             template: path.resolve(__dirname, 'public', 'index.html'),
         })
     ],
+
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: 'babel-loader',
-            }
+            },
+            {
+                test: /\.css|scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
         ]
     },
 };
