f6c5452fa09843e109d638756703daf0704e4e18
unknown <leonardosanttos0077@gmail.com>
2021-03-16 14:43:10 -0300

    add styled-components

--- ex26/src/App.tsx
+++ ex26/src/App.tsx
@@ -1,7 +1,11 @@
+import { GlobalStyle } from './styles/global'
+
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+   <>
+    <GlobalStyle/>
+
+    <h1>Hello</h1>
+   </>
   );
 }
---
+++ ex26/src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        font-family: sans-serif;
+        color: #4d4d4d;
+    }
+
+`
